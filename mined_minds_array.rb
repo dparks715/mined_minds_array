a9680a71aacfa862c1159a68988c0234c8685597
Daniel Parks <dparks715@yahoo.com>
2017-01-19 14:52:33 -0500

    first commit

---
+++ mined_minds_array.rb
@@ -0,0 +1,10 @@
+def create_mined_minds_array()
+	 #create empty array, but defined it as follows
+	mined_minds_array = [] #now populate 100x
+	100.times do
+		#push itemsinto array
+		mined_minds_array << 'array item'
+	end
+#the previous fills the array witih 100 items, then 
+	mined_minds_array
+end
