c38b96dd847f64f26d6c10de14ff061c4a483301
Daniel Parks <dparks715@yahoo.com>
2017-01-20 07:45:11 -0500

    test 3 is mined

--- test_mined_minds_array.rb
+++ test_mined_minds_array.rb
@@ -16,4 +16,12 @@ class TestMMFunctions < Minitest::Test
 		results = create_mined_minds_array()
 		assert_equal(100, results.length)
 	end
+
+	# first item in array is located at index 0
+	# so 3 is located at index 2, 5 at 4, etc.
+	def test_function_returns_mined()
+		results = create_mined_minds_array()
+		assert_equal('mined', results[2])
+	end
+
 end
